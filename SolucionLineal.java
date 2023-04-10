886ef309b7171dc09deb747663ddc2c3a37fa3a5
CrisAcosta1705 <128009007+CrisAcosta1705@users.noreply.github.com>
2023-04-09 23:08:38 -0600

    Add files via upload

---
+++ SolucionLineal.java
@@ -0,0 +1,16 @@
+package SolucionLineal;
+
+public class SolucionLineal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double a = 2.0; // Reemplazar con el valor de a
+		double b = 5.0; // Reemplazar con el valor de b
+
+		double x = -b/a;
+
+		System.out.println("La solución lineal es: " + x);
+	}
+
+}
